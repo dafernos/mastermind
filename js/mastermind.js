b94e63e54c22d23e30f2171e6dc02d9a3ffe01ea
Dario Fernandez <drfernos@gmail.com>
2018-05-31 12:37:30 +0200

    bolas blancas no se mostraban

--- js/mastermind.js
+++ js/mastermind.js
@@ -1,11 +1,8 @@
 {
 	let colores=["black","white","blue","red","yellow","green","purple","brown"];
-	let numBolas = 4;
+	const NUM_BOLAS = 4;
 	var mastermind=(function(){
 		let combinacion=[];
-		let negras,blancas;
-		let posicionbn=[];
-
 		let init = function(){
 			generaCombinacion();
 			mostrar();
@@ -17,44 +14,38 @@
 		//genera una combinación de negros (número de bolas que están en su sitio) y blancos (número de bolas que estám, pero no en su sitio).
 		let comprobarIntento=function(intento){
 			let salir;
-			posicionbn.length=0;
 			let copiacombinacion=combinacion.slice();
 			//negros(en su sitio)
-			negras=0; 
-			for (let i = 0; i <= intento.length-1; i++) {
+			let blancas=0;
+			let negras=0; 
+			for (let i = 0; i < intento.length; i++) {
 				if(intento[i]===copiacombinacion[i]){
 					negras++; 
 					copiacombinacion[i]=undefined;
 					intento[i]=null;
-					posicionbn[i]="n";
 				}
 			}
 			//blancos(estan en otra posicion)
-			blancas=0;
-			for(let i=0; i <= intento.length-1;i++){
+			for(let i=0; i < intento.length;i++){
 				salir = false;
-				for (let  j= 0; j <= copiacombinacion.length-1; j++) {
+				for (let  j= 0; j < copiacombinacion.length; j++) {
 					if(copiacombinacion[j]===intento[i] && salir===false){
 						blancas++;
 						copiacombinacion[j]=undefined;
-						
-						posicionbn[j]="b";
-						salir=true;
+						break;
 					}
 				}
 			}
 			return {
 				negras: negras,
-				blancas: blancas,
-				copiacombinacion:copiacombinacion,
+				blancas: blancas
 			}
 		};
 		//genera una combinación.
 		let generaCombinacion = function(){
-			negras=0;
-			blancas=0;
+			blancas=negras=0;
 			if(combinacion) combinacion=[];
-			for (let i = 0; i < numBolas; i++) {
+			for (let i = 0; i < NUM_BOLAS; i++) {
 				combinacion.push(colores[parseInt(Math.random()*8)]);
 			}
 		}
@@ -93,31 +84,37 @@
 			let aciertosVacios = document.createElement('div')
 			aciertosVacios.setAttribute('class',' wrapper cuatro');
 			//creamos las bolas para colocar y los aciertos
-			for (let i = 0; i < numBolas; i++) {
+			for (let i = 0; i < NUM_BOLAS; i++) {
 				bola = document.createElement("div");
 				bola.setAttribute('class',"circle bola"+conFila);
 				bolasVacias.appendChild(bola);
 				acierto = document.createElement("div");
 				acierto.setAttribute('class',"circle acierto"+conFila);
 				aciertosVacios.appendChild(acierto);
-			}
+			};
 			conFila++;
 			filaNueva.appendChild(bolasVacias);
 			filaNueva.appendChild(aciertosVacios);
 			document.getElementById('filas').appendChild(filaNueva);
-		}
+		};
 		//Quitar bola de la fila
 		let quitarBola =function (event) {
 			event.target.setAttribute('style','background:""');
 			event.target.removeEventListener("click", quitarBola);
 			conBola--;
-		}
+		};
 		//Poner bola en la fila
 		let ponerBola=function (event) {
+			let btncomprobar = document.getElementById('comprobar')
 			colorSeleccionado = event.target.style.backgroundColor;
+			/*if (conBola==4) {
+				btncomprobar.removeAttr('disabled');
+			}else{
+				btncomprobar.setAttribute('disabled',true);
+			}*/
 			for (let i = 0; i <= fila.length-1 ; i++) {
 				if (fila[i].style.background === ""){
-					fila[i].setAttribute('style','background:'+colorSeleccionado);
+					fila[i].style.background =colorSeleccionado;
 					fila[i].addEventListener("click", quitarBola);
 					conBola++;
 					break;
@@ -132,45 +129,31 @@
 			for (let i = 0; i <= fila.length - 1; i++) {
 				combinacion.push(fila[i].style.backgroundColor);
 			}
-			if (conBola ==4) {
+			if (conBola==4) {
 				aciertos = mastermind.comprobarIntento(combinacion);
 				//Poner aciertos (bolas negras)
 				if (aciertos.negras > 0 ){
-					console.log(aciertos.negras);
 					for (let i = 0; i < aciertos.negras; i++) {
 						bolAcierto[i].style.background = "black";
 					};
 				};
 				//Poner aciertos (bolas blancas)
 				if (aciertos.blancas > 0){
-					console.log(aciertos.blancas);
 					for (let i = 0; i < aciertos.blancas; i++) {
-						sumBlanc=aciertos.negras+i;
-						bolAcierto[sumBlanc].setAttribute('style','background: white');
+						bolAcierto[aciertos.negras+i].style.background= 'white';
 					};
 				}; 
 				//si hay 4 aciertos o llega a 7 intentos se finaliza el juego
-				if(aciertos.negras === numBolas || conFila===7){
+				if(aciertos.negras === NUM_BOLAS){
 					document.getElementById('ganado').setAttribute('style','display:block');
-					let adivinar = document.getElementById('conad');
-					if (conFila===7) {
-						
-						bola =document.getElementsByClassName('boladi');
-						for (let i = 0; i < numBolas; i++) {
-							bola[i].setAttribute('style','background:'+aciertos.copiacombinacion[i]);
-						}
-						document.getElementById('finTexto').innerHTML='<h1>Has Perdido</h1>';
-					}else{
-						document.getElementById('finTexto').innerHTML='<h1>Has Ganado</h1>';
-					}
-					adivinar.setAttribute('style','display:block');
+					document.getElementById('finTexto').innerHTML='<h1>Has Ganado</h1>';
 					quitarEvento();
 				}else{
 					quitarEvento();
 					crearFila();
 				}
+				conBola=0;
 			}
-			conBola=0;
 		}
 		let quitarEvento = function(){
 			for (let i = 0; i <= fila.length-1 ; i++) {
@@ -188,16 +171,16 @@
 	var init=function(){
 		mastermind.init();
 		comportamiento.crearFila();
-		document.forms[0].elements[0].removeEventListener('click',init);
+		bolasColores();
 	}
-	var reiniciar= function(){
-		document.forms[0].elements[0].addEventListener('click',init);
+
+	var reiniciar= function(){ 
 		document.getElementById('filas').innerHTML="";
-		document.getElementById('ganado').setAttribute('style','display:none');
+		document.getElementById('ganado').style.display='none';
+		init();
 	}
-	window.addEventListener('load',bolasColores);
-	document.forms[0].elements[0].addEventListener('click',init);
-	document.forms[0].elements[1].addEventListener('click',reiniciar);
+
+	window.addEventListener('load',init);
 	document.getElementById('fin').addEventListener('click',reiniciar);
 	document.getElementById('comprobar').addEventListener('click',comportamiento.comprobarFila);
 }
